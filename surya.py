b3571958b76fc0122bf02a81e4ae41d45b78ec05
suryachaganti9 <suryachaganti@gmail.com>
2020-06-21 13:46:39 +0000

    commiting from diffrent branch

---
+++ surya.py
@@ -0,0 +1,15 @@
+
+
+print("this is python program")
+a=4
+b=8
+
+print("a+b sum :",a+b)
+print("a-b sub :",a-b)
+print("a*b mul :",a*b)
+print("a/b div :",a/b)
+
+
+print("Thank you and have nice day !!")
+
+
